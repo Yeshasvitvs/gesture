9744046ece290307d44a521df6413e7a1dd9aeac
yeshi <yeshi@sexpistols.(none)>
2015-04-10 13:40:08 +0200

    Working but with very precise values and the pointing algorithm has to be tailored

--- src/gesture_node.cpp
+++ src/gesture_node.cpp
@@ -17,6 +17,9 @@ bool left_success;
 bool right_success;
 double left_planning_time;
 double right_planning_time;
+bool action;
+int arm_at;
+bool arm_pointing;
 
 geometry_msgs::Pose targetPose; //Target pose variable
 ros::Subscriber gesture_sub; //Gesture subscriber
@@ -39,114 +42,157 @@ int main(int argc, char** argv)
 	ros::NodeHandle nh;
 	ros::AsyncSpinner spinner(1); // Use 4 threads
     	spinner.start();
+    	action = false; 
+    	
+    	gesture_sub = nh.subscribe<std_msgs::Char>("/gesture_detected",1,gestureCallback);
    	
 	//Move group variables
 	moveit::planning_interface::MoveGroup left_group("left_arm");
 	moveit::planning_interface::MoveGroup right_group("right_arm");
+	
+	//Planning parameters 
+	left_group.setNumPlanningAttempts(4);
+	left_group.setPlanningTime(5.0);
+	
+	right_group.setNumPlanningAttempts(4);
+	right_group.setPlanningTime(5.0);
+	
+	
+	//Changing default planner to new one
+	//left_group.setPlannerId("SBLkConfigDefault");
+	//right_group.setPlannerId("SBLkConfigDefault");
 
 	//Plan variables
 	moveit::planning_interface::MoveGroup::Plan left_plan;
 	moveit::planning_interface::MoveGroup::Plan right_plan;
 	
-	moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+	//moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-	gesture_sub = nh.subscribe<std_msgs::Char>("/gesture_detected",1,gestureCallback);
+	left_planning_time = 0;
+	right_planning_time = 0;
+	arm_pointing = false;
+	
 		
 	ros::Rate rate(10);
-	while(ros::ok())
+	while(ros::ok()) //TODO check if this is getting executed 
 	{
 	
+		
 		std::cout << "Inside ROS OK!" << std::endl;//Debug Code
-  		left_planning_time = 0;
-		right_planning_time = 0;	
+  			
 		
-		//right_group.setRandomTarget();
-		//right_group.move();  	
 		
-  		cout<<"Have to perform gesture" << endl;
+  		cout<<"Waiting for gesture..." << endl;
   	
  		if(p == 65)//char A
  		{ 
  			std::cout<<"Received Gesture A "<<std::endl;//Debug Code
  			
- 			targetPose.position.x=0.8;	//Set the end effector pose values for each gesture
-			targetPose.position.y=0.3;
-			targetPose.position.z=-0.1;
-			//targetPose.orientation.x=0.0115213171449;
-			//targetPose.orientation.y=0.999834934573;
-			//targetPose.orientation.z=0.0139595637967;
-			targetPose.orientation.w=1.0;
+ 			targetPose.position.x=0.803649419459;	//Set the end effector pose values for each gesture
+			targetPose.position.y=0.276442348416;
+			targetPose.position.z=-0.157090056159;
+			targetPose.orientation.x=0.879319704916;
+			targetPose.orientation.y=0.281886732657;
+			targetPose.orientation.z=0.359421220203;
+			targetPose.orientation.w=-0.134733488657;
+			arm_at = p;
+			p=0;
+			action = true;
 			
-	}
+		}
   	
 		else if(p == 66)//char B
  		{ 
  			std::cout<<"Received Gesture B "<<std::endl;//Debug Code
- 			targetPose.position.x=0.80;	//Set the end effector pose values for each gesture
-			targetPose.position.y=-0.00;
-			targetPose.position.z=-0.17;
-			targetPose.orientation.x=0.0;
-			targetPose.orientation.y=-0.0;
-			targetPose.orientation.z=0.0;
-			targetPose.orientation.w=-0.0;
+ 			targetPose.position.x=0.717970866314;	//Set the end effector pose values for each gesture
+			targetPose.position.y=0.0100056636139;
+			targetPose.position.z=-0.150521111908;
+			targetPose.orientation.x=0.325363778735;
+			targetPose.orientation.y=0.945401924539;
+			targetPose.orientation.z=0.00738698618437;
+			targetPose.orientation.w=0.0172929176549;
+			arm_at = p;
+			p=0;
+			action = true;
  	 	}
 		else if(p == 67)//char C
  		{ 
  			std::cout<<"Received Gesture C "<<std::endl;//Debug Code
- 			targetPose.position.x=0.69;	//Set the end effector pose values for each gesture
-			targetPose.position.y=-0.35;
-			targetPose.position.z=-0.19;
-			targetPose.orientation.x=0.0;
-			targetPose.orientation.y=0.0;
-			targetPose.orientation.z=0.0;
-			targetPose.orientation.w=0.0;
+ 			targetPose.position.x=0.700446656582;	//Set the end effector pose values for each gesture
+			targetPose.position.y=-0.307275652487;
+			targetPose.position.z=-0.148630257636;
+			targetPose.orientation.x=-0.514525139783;
+			targetPose.orientation.y=0.753406720702;
+			targetPose.orientation.z=-0.405288697056;
+			targetPose.orientation.w=-0.0581658471183;
+			arm_at = p;
+			p=0;
+			action = true;
  	 	}
 
 
-	
-  		left_group.setPoseTarget(targetPose);
-  		right_group.setPoseTarget(targetPose);
+		if(action == true ){
+		
+  			left_group.setPoseTarget(targetPose);
+  			left_group.setGoalPositionTolerance(0.1);
+			left_group.setGoalOrientationTolerance(0.4); 
+			
+  			right_group.setPoseTarget(targetPose);
+  			right_group.setGoalPositionTolerance(0.1);
+			right_group.setGoalOrientationTolerance(0.4); 
   		
-  		left_planning_time = ros::Time::now().toSec();
-		left_success = left_group.plan(left_plan);
-		std::cout << "Left plan success : " << left_success << std::endl;//Debug Code
-		left_planning_time = ros::Time::now().toSec() - left_planning_time;
-		
-  		right_planning_time = ros::Time::now().toSec();
-		right_success = right_group.plan(right_plan);
-		std::cout << "Right plan success : " << right_success << std::endl;//Debug Code
-		right_planning_time = ros::Time::now().toSec() - right_planning_time;
-
-
-		std::cout << "Left planning time "<< left_planning_time << std::endl;//Debug Code
-		std::cout << "Right planning time "<< right_planning_time << std::endl;//Debug Code
-		
-		if( left_success == false && right_success == false ){
-
-			std::cout << "Moving right arm to Random pose "<<std::endl;
-			//right_group.setRandomTarget();
-			//right_group.move();
-		}
-		else if(right_planning_time < left_planning_time){
-
-			if(right_success == true ){ 
+  			left_planning_time = ros::Time::now().toSec();
+			left_success = left_group.plan(left_plan);
+			left_planning_time = ros::Time::now().toSec() - left_planning_time ;
 			
-				right_group.move(); 
+			std::cout << "Left plan success : " << left_success << std::endl;//Debug Code
+			std::cout << "Left planning time "<< left_planning_time << std::endl;//Debug Code
+		
+  			right_planning_time = ros::Time::now().toSec();
+			right_success = right_group.plan(right_plan);
+			right_planning_time = ros::Time::now().toSec() - right_planning_time ;
+			
+			std::cout << "Right plan success : " << right_success << std::endl;//Debug Code
+			std::cout << "Right planning time "<< right_planning_time << std::endl;//Debug Code
+			
+		
+			if( left_success == false && right_success == false ){
+		
+				std::cout << "Moving right arm to Random pose "<<std::endl;
+				//right_group.setRandomTarget();
+				//right_group.move();
+			}
+			else if(right_planning_time < left_planning_time){
+
+				if(right_success == true ){ 
+				
+					right_group.move(); 
+					action = false;
+				
+				}
+				else { 
+					left_group.move(); 
+					action = false;
+				}
 			
 			}
-			else { 
-				left_group.move(); 
-			}
-		
+			else{ 
+				
+  			  	if(left_success == true ){ 
+  			  		left_group.move(); 
+  			  		action = false;
+  			  	}
+				else { 
+					right_group.move(); 
+					action = false;
+				}
+  			}
+  		
+  		
+  		
 		}
-		else{ 
-			
-  		  	if(left_success == true ){ left_group.move(); }
-			else { right_group.move(); }
-  		}
-  	
-  	rate.sleep() ;
-  	ros::waitForShutdown();
+	rate.sleep() ;
 	}
+	ros::waitForShutdown();
 	return 0;
-}	
+}		
